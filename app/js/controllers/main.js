71e7d9f5279f3d56ee76860028b1e120a25d45b4
shhubhamsharma <shubhams2@kpit.com>
2018-01-29 19:25:11 +0530

    commit

--- app/js/controllers/main.js
+++ app/js/controllers/main.js
@@ -18,10 +18,9 @@ $scope.seErr=false;
           }
         }
       }
-      console.log($scope.selectedItem);
     },
     function error(error) {
-      console.log(error);
+$scope.seErr=true;
     }
   );
   $scope.labelsBar = ['Total',];
